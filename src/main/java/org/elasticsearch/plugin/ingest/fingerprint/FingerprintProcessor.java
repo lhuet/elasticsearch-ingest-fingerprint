48cd1a00ba23501c4612c7c03d54ba1e1bffe147
Laurent Huet <laurent.huet@gmail.com>
2019-10-01 21:50:50 +0200

    add first ingest fingerprint version

---
+++ src/main/java/org/elasticsearch/plugin/ingest/fingerprint/FingerprintProcessor.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright [2019] [Laurent HUET]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.elasticsearch.plugin.ingest.fingerprint;
+
+import org.elasticsearch.ingest.AbstractProcessor;
+import org.elasticsearch.ingest.IngestDocument;
+import org.elasticsearch.ingest.Processor;
+
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+
+import static org.elasticsearch.ingest.ConfigurationUtils.*;
+
+public class FingerprintProcessor extends AbstractProcessor {
+
+    public static final String TYPE = "fingerprint";
+
+    private final String target;
+    private final List<String> source;
+    private final String method;
+
+    public FingerprintProcessor(String tag, String target, List<String> source, String method) {
+        super(tag);
+        this.target = target;
+        this.source = source;
+        this.method = method;
+    }
+
+    @Override
+    public IngestDocument execute(IngestDocument ingestDocument) throws Exception {
+
+        // TODO: [Performance] Use a MessageDisgest pool object to prevent creating a new one for each document
+        MessageDigest md = MessageDigest.getInstance(this.method);
+
+
+        for (String field: source) {
+            md.update(ingestDocument.getFieldValue(field, String.class).getBytes("UTF-8"));
+        }
+        byte[] hash = md.digest();
+
+        if (target.equals("_id")) {
+            ingestDocument.getSourceAndMetadata().put(IngestDocument.MetaData.ID.getFieldName(),Base64.getEncoder().encodeToString(hash));
+        }
+        else {
+            ingestDocument.setFieldValue(target, Base64.getEncoder().encodeToString(hash));
+        }
+
+        return ingestDocument;
+    }
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public List<String> getSource() {
+        return source;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public static final class Factory implements Processor.Factory {
+
+        @Override
+        public FingerprintProcessor create(Map<String, Processor.Factory> processorFactories, String tag, Map<String, Object> config)
+                throws Exception {
+            // Target is optional. If not set, we put the hash value as the document Id
+            String target = readOptionalStringProperty(TYPE, tag, config, "target_field");
+            if (target == null) target = "_id";
+            // Field is optional. If not set, we used all the field values sorted by lexical order
+            List<String> source = readList(TYPE, tag, config, "field");
+            if (source == null) source = Arrays.asList("_all");
+            // Method is optional. Use SHA-1 by default
+            String method = readOptionalStringProperty(TYPE, tag, config, "method");
+            if (method == null) {
+                method = Method.SHA1.getAlgorithm();
+            }
+            else {
+                method = Method.fromString(method).getAlgorithm();
+            }
+
+            return new FingerprintProcessor(tag, target, source, method);
+        }
+    }
+
+    enum Method {
+        MD5("MD5"),
+        SHA1("SHA-1"),
+        SHA224("SHA-224"),
+        SHA256("SHA-256"),
+        SHA384("SHA-384"),
+        SHA512("SHA-512");
+
+        private String algorithm;
+
+        Method(String algorithm) {
+            this.algorithm = algorithm;
+        }
+
+        public String getAlgorithm() {
+            return this.algorithm;
+        }
+
+        public static Method fromString(String method) {
+            try {
+                return Method.valueOf(method);
+            }
+            catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("Method '" + method + "' not supported", e);
+            }
+
+        }
+    }
+
+}
---
+++ src/test/java/org/elasticsearch/plugin/ingest/fingerprint/FingerprintProcessorTests.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright [2019] [Laurent HUET]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.elasticsearch.plugin.ingest.fingerprint;
+
+import org.elasticsearch.ingest.IngestDocument;
+import org.elasticsearch.ingest.RandomDocumentPicks;
+import org.elasticsearch.test.ESTestCase;
+import org.junit.BeforeClass;
+import org.elasticsearch.plugin.ingest.fingerprint.FingerprintProcessor;
+
+import java.security.MessageDigest;
+import java.util.*;
+
+public class FingerprintProcessorTests extends ESTestCase {
+
+    private static Map<String, Object> defaultTestDoc;
+
+    @BeforeClass
+    public static void defaultDoc() {
+        defaultTestDoc = new HashMap<>();
+        defaultTestDoc.put("message", "my test string value");
+        defaultTestDoc.put("asecondmessage", "my second test string value");
+
+    }
+
+    public void testSHA1SourceFieldOnTargetField() throws Exception {
+
+        List<String> sourceField = new ArrayList<>();
+        sourceField.add("message");
+
+        IngestDocument resultedDoc = helperFingerprintProcessorExecute("hash", sourceField, "SHA1");
+
+        logger.info("Document after processor: "  + resultedDoc.toString());
+
+        byte[] sha1msg = MessageDigest.getInstance("SHA-1")
+                .digest(resultedDoc.getFieldValue("message", String.class).getBytes("UTF-8"));
+
+        String processorSha1 = resultedDoc.getFieldValue("hash", String.class);
+        String computedSha1 = Base64.getEncoder().encodeToString(sha1msg);
+
+        assertTrue(processorSha1.equals(computedSha1));
+    }
+
+    public void testSHA1SourceFieldOnIdField() throws Exception {
+
+        List<String> sourceField = new ArrayList<>();
+        sourceField.add("message");
+
+        IngestDocument resultedDoc = helperFingerprintProcessorExecute("_id", sourceField, "SHA-1");
+
+        logger.info("Document after processor: "  + resultedDoc.toString());
+
+        byte[] sha1msg = MessageDigest.getInstance("SHA-1")
+                .digest(resultedDoc.getFieldValue("message", String.class).getBytes("UTF-8"));
+
+        String processorSha1 = (String) resultedDoc.getSourceAndMetadata().get(IngestDocument.MetaData.ID.getFieldName());
+        String computedSha1 = Base64.getEncoder().encodeToString(sha1msg);
+
+        assertTrue(processorSha1.equals(computedSha1));
+
+    }
+
+    public void testSHA256SourceFieldOnIdField() throws Exception {
+
+        List<String> sourceField = new ArrayList<>();
+        sourceField.add("asecondmessage");
+        sourceField.add("message");
+        IngestDocument resultedDoc = helperFingerprintProcessorExecute("_id", sourceField, "SHA-256");
+
+        logger.info("Document after processor: "  + resultedDoc.toString());
+
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        md.update(resultedDoc.getFieldValue("asecondmessage", String.class).getBytes("UTF-8"));
+        md.update(resultedDoc.getFieldValue("message", String.class).getBytes("UTF-8"));
+        byte[] sha1msg = md.digest();
+
+        String processorSha256 = (String) resultedDoc.getSourceAndMetadata().get(IngestDocument.MetaData.ID.getFieldName());
+        String computedSha1 = Base64.getEncoder().encodeToString(sha1msg);
+
+        assertTrue(processorSha256.equals(computedSha1));
+
+    }
+
+    private IngestDocument helperFingerprintProcessorExecute(String targetField, List<String> sourceField, String algorithm) throws Exception {
+
+        IngestDocument ingestDoc = RandomDocumentPicks.randomIngestDocument(random(), defaultTestDoc);
+
+        FingerprintProcessor fingerprintProcessor =
+                new FingerprintProcessor(randomAlphaOfLength(10), targetField, sourceField, algorithm);
+
+        return fingerprintProcessor.execute(ingestDoc);
+    }
+}
